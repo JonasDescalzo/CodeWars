a0f67185172daf9ea656b639fb9aeea9e8b3a3f5
Jonas <jonas.netcore.konganbuddies@gmail.com>
2021-10-14 17:51:11 +0800

    Added new files

--- CodeWars/Service/SolutionService.cs
+++ CodeWars/Service/SolutionService.cs
@@ -521,38 +521,5 @@ namespace CodeWars.Service
         }
         #endregion
 
-        #region Challennge #21: Alphabetic Anagrams
-        //Alphabetic Anagrams
-        public long ListPosition(string value)
-        {
-            var wordlist = Permutation(value.ToLower());
-
-            wordlist.Sort();
-
-            long position = wordlist.IndexOf(value.ToLower()) + 1;
-
-            return position;
-        }
-
-        public static List<string> Permutation(string s)
-        {
-            if (s.Length < 2)
-            {
-                return new List<string> { s };
-            }
-            var result = new HashSet<string>();
-            foreach (var sub in Permutation(s.Substring(1)))
-            {
-                for (int i = 0; i <= sub.Length; i++)
-                {
-                    result.Add(sub.Substring(0, i) + s[0] + sub.Substring(i));
-                }
-
-                var test = sub;
-            }
-            return result.ToList();
-        }
-        #endregion
-
     }
 }
---
+++ CodeWars/Service/SolutionService2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using CodeWars.Helpers;
+
+namespace CodeWars.Service
+{
+    public class SolutionService2
+    {
+        #region Challennge #21: Alphabetic Anagrams
+        //Alphabetic Anagrams
+        public long ListPosition(string value)
+        {
+            var wordlist = Permutation(value.ToLower());
+
+            wordlist.Sort();
+
+            long position = wordlist.IndexOf(value.ToLower()) + 1;
+
+            return position;
+        }
+
+        public static List<string> Permutation(string s)
+        {
+            if (s.Length < 2)
+            {
+                return new List<string> { s };
+            }
+            var result = new HashSet<string>();
+            foreach (var sub in Permutation(s.Substring(1)))
+            {
+                for (int i = 0; i <= sub.Length; i++)
+                {
+                    result.Add(sub.Substring(0, i) + s[0] + sub.Substring(i));
+                }
+
+                var test = sub;
+            }
+            return result.ToList();
+        }
+        #endregion
+    }
+}
