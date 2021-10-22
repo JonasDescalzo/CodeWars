b843866d013eb0fd284f33543ac70ef580237594
Jonas <jonas.netcore.konganbuddies@gmail.com>
2021-10-22 11:03:36 +0800

    Solution to challenges 23, 24, and 25

--- CodeWars/Controllers/Challenges2Controller.cs
+++ CodeWars/Controllers/Challenges2Controller.cs
@@ -40,5 +40,38 @@ namespace CodeWars.Controllers
             return Ok(response);
         }
         #endregion
+
+        #region Challennge #23: Twice linear
+        //Twice linear
+        [HttpPost("twice-linear/{number}")]
+        public IActionResult TwiceLinear(int number)
+        {
+            var response = _solutionService2.DblLinear(number);
+
+            return Ok(response);
+        }
+        #endregion
+
+        #region Challennge #24: Square into Squares. Protect trees!
+        //Square into Squares. Protect trees!
+        [HttpPost("decompose-square/{number}")]
+        public IActionResult Decompose(long number)
+        {
+            var response = _solutionService2.Decompose(number);
+
+            return Ok(response);
+        }
+        #endregion
+
+        #region Challennge #25: Strings Mix
+        //Strings Mix
+        [HttpPost("strings-mix/{s1}/{s2}")]
+        public IActionResult Decompose(string s1, string s2)
+        {
+            var response = _solutionService2.Mix(s1, s2);
+
+            return Ok(response);
+        }
+        #endregion
     }
 }
--- CodeWars/Service/SolutionService2.cs
+++ CodeWars/Service/SolutionService2.cs
@@ -10,7 +10,7 @@ namespace CodeWars.Service
 {
     public class SolutionService2
     {
-        #region Challennge #21: Alphabetic Anagrams
+        #region Challennge #21: Alphabetic Anagrams [TIMEOUT]
         //Alphabetic Anagrams
         public long ListPosition(string value)
         {
@@ -47,72 +47,10 @@ namespace CodeWars.Service
         //Next smaller number with the same digits
         public long NextSmaller(long n)
         {
-            #region Solution 1
-            //var permutations = Permutation(n.ToString()).Select(c => Convert.ToInt64(c)).ToList();
-
-            //if (permutations.Count == 1)
-            //{
-            //    return -1;
-            //}
-
-            //permutations.Sort();
-
-            //permutations = permutations.Where(c => c.ToString().Length == n.ToString().Length).ToList();
-
-            //var index = permutations.IndexOf(n);
-
-            //var result = index == 0 ? -1 : permutations[index - 1];
-
-            //return result;
-            #endregion
-
-            #region Solution 2
-            //var digits = Array.ConvertAll(n.ToString().ToCharArray(), c => (int)Char.GetNumericValue(c));
-
-            //var leftHalf = new List<int>();
-            //var rightHalf = new List<int>();
-
-
-            //for (int i = digits.Count() - 1; i >= 0; i--)
-            //{
-            //    if (i == 0)
-            //    {
-            //        break;
-            //    }
-
-            //    if (digits[i] < digits[i - 1])
-            //    {
-            //        leftHalf = digits.Take(i - 1).ToList();
-            //        rightHalf = digits.Skip(i - 1).ToList();
-            //        break;
-            //    }
-            //}
-
-            //var temp = Convert.ToInt64(String.Join("", rightHalf));
-
-            //var permutations = Permutation(temp.ToString()).Select(c => Convert.ToInt64(c)).ToList();
-
-            //if (permutations.Count == 1)
-            //{
-            //    return -1;
-            //}
-
-            //permutations.Sort();
-
-            //permutations = permutations.Where(c => c.ToString().Length == temp.ToString().Length).ToList();
-
-            //var index = permutations.IndexOf(temp);
-
-            //var result = index == 0 ? -1 : permutations[index - 1];
-
-            //var last = Convert.ToInt64(String.Join("", leftHalf) + result);
-
-            //return last;
-            #endregion
-
-            #region Solution 3 ***
+            //int[] digits
             var digits = Array.ConvertAll(n.ToString().ToCharArray(), c => (int)Char.GetNumericValue(c));
-
+            
+            //List<int> sorted
             var sorted = digits.Distinct().ToList();
             sorted.Sort();
 
@@ -145,8 +83,9 @@ namespace CodeWars.Service
                 return -1;
             }
 
+            //long temp
             var temp = Convert.ToInt64(String.Join("", rightHalf));
-
+            //List<string> permutations
             var permutations = Permutation(temp.ToString()).ToList();
 
             if (permutations.Count == 1)
@@ -157,9 +96,9 @@ namespace CodeWars.Service
             permutations.Sort();
 
             permutations = permutations.Where(c => c.ToString().Length == temp.ToString().Length).ToList();
-
+            //int index
             var index = permutations.IndexOf(temp.ToString());
-
+            //string result
             var result = index == 0 ? "-1" : permutations[index - 1];
 
             if (result == "-1")
@@ -170,7 +109,176 @@ namespace CodeWars.Service
             var last = Convert.ToInt64(String.Join("", leftHalf) + result);
 
             return last;
-            #endregion
+        }
+        #endregion
+
+        #region Challennge #23: Twice linear [TIMEOUT]
+        //Twice linear
+        public int DblLinear(int n)
+        {
+            /*
+            var Linears = new List<int>() { 1 };
+
+            for (int i = 0; i <= n + 1; i++)
+            {
+                var Y = 2 * Linears[i] + 1; 
+                var Z = 3 * Linears[i] + 1;
+
+                Linears.AddRange(new List<int>() { Y, Z });
+
+                Linears = Linears.Distinct().ToList();
+                Linears.Sort();
+            }
+
+            return Linears[n];
+            */
+
+            var Linears = new List<int>() { 1 };
+
+            for (int i = 0; i <= n + 1; i++)
+            {
+                var Y = 2 * Linears[i] + 1;
+                var Z = 3 * Linears[i] + 1;
+
+                Linears.AddRange(new List<int>() { Y, Z });
+
+                Linears = Linears.Distinct().ToList();
+                Linears.Sort();
+            }
+
+            return Linears[n];
+        }
+        #endregion
+
+        #region Challennge #24: Square into Squares. Protect trees! [UNSOLVED]
+        //Square into Squares. Protect trees!
+        public string Decompose(long n)
+        {
+            var squared = n * n;
+
+            var result = new List<int>();
+
+            var squares = new int[n - 1];
+
+            for (int i = 1; i < n; i++)
+            {
+                squares[i - 1] = i; 
+            }
+
+            for (int i = squares.Length - 1 ; i >= 0; i--)
+            {
+                if(squares[i] * squares[i] <= squared)
+                {
+                    result.Add(squares[i]);
+                    squared -= squares[i] * squares[i];
+                }
+            }
+
+            result.Reverse();
+
+            var final_result = squared != 0 ? null : String.Join(" ", result);
+
+            return final_result;
+        }
+
+        #endregion
+
+        #region Challennge #25: Strings Mix
+        //Strings Mix
+        public string Mix(string s1, string s2)
+        {
+            var s1CharList = GetCharList(s1, "1:");
+            var s1Strings = s1CharList.Select(c => c[2]).ToList();
+
+            var s2CharList = GetCharList(s2, "2:");
+            var s2Strings = s2CharList.Select(c => c[2]).ToList();
+
+            var finalS1 = Finalize(s1Strings, s2Strings, s1CharList);
+            var finalS2 = Finalize(s1Strings, s2Strings, s2CharList);
+
+            var final = finalS1;
+
+            for (int i = 0; i < final.Count; i++)
+            {
+                if (final[i].Length == finalS2[i].Length)
+                {
+                    final[i] = finalS1[i].Replace("1", "3");
+                }
+                else if (final[i].Length < finalS2[i].Length)
+                {
+                    final[i] = finalS2[i];
+                }
+            }
+
+            final.Sort();
+
+            final.Reverse();
+
+            final = finalS1.OrderBy(a => a.Length).ToList();
+
+            final.Reverse();
+
+            for (int i = 0; i < final.Count; i++)
+            {
+                final[i] = final[i].Replace('3', '=');
+            }
+
+            var result = String.Join("/", final);
+
+            return result;
+        }
+
+        public List<string> GetCharList(string sentence, string prefix)
+        {
+            //get letters
+            var letters = sentence.Where(c => '`' < c && c < '{').ToList();
+            letters.Sort();
+            //get unique letters
+            var uniqueLetters = letters.Distinct().ToList();
+
+            var characterList = new List<string>();
+
+            foreach (var u in uniqueLetters)
+            {
+                string tempList = "";
+
+                foreach (var character in letters)
+                {
+                    if (u == character)
+                    {
+                        tempList += character;
+                    }
+                }
+
+                if(tempList.Length > 1)
+                {
+                    tempList = prefix + tempList;
+                    characterList.Add(tempList);
+                }
+            }
+
+            return characterList;
+        }
+
+        public List<string> Finalize(List<char> s1List, List<char> s2List, List<string> charList)
+        {
+            var finalized = new List<string>();
+            finalized.AddRange(s1List.Select(c => c.ToString()));
+            finalized.AddRange(s2List.Select(c => c.ToString()));
+
+            finalized = finalized.Distinct().ToList();
+
+            foreach (var item in charList)
+            {
+                var a = finalized.IndexOf(item[2].ToString());
+
+                if (finalized[a].Length < item.Length)
+                {
+                    finalized[a] = item;
+                }
+            }
+
+            return finalized;
         }
         #endregion
     }
